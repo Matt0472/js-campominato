2d4a2e629dc8ff207d8f06efad8de46536da8d08
Mattia Pedone <mattiapedone93@gmail.com>
2020-01-14 17:50:42 +0100

    inizio corpo centrale del programma

--- campominato/javascript/script.js
+++ campominato/javascript/script.js
@@ -5,37 +5,37 @@
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
 var arrayRandomNumber = [];
+var arrayUserTries = [];
 var findNumber = false;
-for (var i = 1; i <= 16; i++) {
+var maxTries = 84;
+
+while (arrayRandomNumber.length < 16) {
   var cpuRandomNumber = getRandomNumber(1, 100);
-  arrayRandomNumber.push(cpuRandomNumber);
-  console.log(arrayRandomNumber);
-}
-
-for (var j = 1; j <= 16; j++) {
-  var userNumbers = parseInt(prompt('Inserisci un numero da 1 a 100'));
-  console.log(userNumbers);
-}
-
-var g = 0;
-while (g < arrayRandomNumber.length && trovato == false) {
-  var position = arrayRandomNumber[i];
-  if (userNumbers != position) {
-    findNumber = true;
-    console.log('Hai perso');
-  } else {
-    console.log('Hai vinto');
+  if (arrayRandomNumber.includes(cpuRandomNumber) == false) {
+    arrayRandomNumber.push(cpuRandomNumber);
   }
-  g++
 }
 
+function checkBomb(arrayToCheck, numberToCheck) {
+  for (var i = 0; i < arrayToCheck.length; i++) {
+    if (numberToCheck == arrayToCheck[i]) {
+      return true;
+    }
+  }
+  return false;
+}
 
-
-
-
-
-
-
+var counter = 0;
+while (arrayUserTries.length < maxTries && checkBomb(arrayRandomNumber, userTry) == false) {
+  var userTry = parseInt(prompt('Inserisci un numero da 1 a 100'));
+  // controllo che sia una bomba(checkBomb)
+    //se è una bomba il gioco è finito, indico il punteggio
+  // controllo che non ho già detto quel number
+    // aggiungo il tentativo all'array dei tentativi
+    // aumentare il punteggio
+  // se il numero inserito è uguale ad uno dei numeri precedentemente inseriti, chiediamo di reinserire il numero
+  // se abbiamo raggiunto il max dei tentativi allora abbiamo vinto
+}
 
 
 
